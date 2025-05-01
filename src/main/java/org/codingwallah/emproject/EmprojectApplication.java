74babe80a76aa64473e71ad997ef072b468b5eaf
Mayank <kumar113349@gmail.com>
2025-05-01 23:18:56 +0530

    first

---
+++ src/main/java/org/codingwallah/emproject/EmprojectApplication.java
@@ -0,0 +1,13 @@
+package org.codingwallah.emproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/codingwallah/emproject/EmprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codingwallah.emproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
